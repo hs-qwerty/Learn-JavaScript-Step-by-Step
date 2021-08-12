c75884228dc867f8affde55dbe29a99db325e7b7
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2021-08-13 00:20:33 +0300

    Add files via upload
    Learn JavaScript - Loops

---
+++ jsLoops/operators/operators.js
@@ -0,0 +1,27 @@
+/* operators */
+
+var age = 20;
+
+
+
+if ( age > 15 || age > 30){
+    console.log("age greater than twenty");
+}
+
+var stat = 14;
+
+if ( stat > 5 && stat < 9){
+    console.log("status less than 5");
+}
+
+
+var total = 18;
+var job = "teacher";
+
+if (job === "student" && total > 17){
+    console.log("you can vote");
+}else if (total > 17){
+    console.log("you can get a scholarship");
+}else {
+    console.log("here");
+}
---
+++ jsLoops/while/while.js
@@ -0,0 +1,31 @@
+var x = 1;
+
+/*
+while (x < 10){
+    console.log(x);
+    x++;
+}
+
+*/
+
+/*
+
+do {
+    console.log(x);
+    x++;
+}while (x < 10)
+
+ */
+
+for (x; x <8; x++){
+
+    if (x === 5){
+        console.log("here");
+    }else if (x === 7){
+        break;
+    }
+    console.log(x);
+}
+
+
+
