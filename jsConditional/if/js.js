bfbb3ba32a6be2188fa6ffd3675a7697433583fc
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2021-08-13 00:20:07 +0300

    Add files via upload
    Learn JavaScript - Conditional

---
+++ jsConditional/if/js.js
@@ -0,0 +1,29 @@
+
+/*
+var x = prompt("number ?");
+if (x > 5){
+    document.bgColor = "blue";
+    console.log("x then 5");
+}else if (x < 4){
+    document.bgColor = "red";
+    console.log("x better 5");
+}else{
+    document.bgColor = "black";
+    console.log("x equals 5");
+}
+
+
+*/
+var y  = false;
+
+if ( y ){
+    document.bgColor = "yellow";
+}else{
+    document.bgColor = "purple";
+}
+
+
+
+
+
+
---
+++ jsConditional/switch/switch.js
@@ -0,0 +1,13 @@
+var job = "soldier";
+
+switch (job){
+    case "art":
+        console.log("I am a soldier");
+        break;
+    case "teacher":
+        console.log("ı am a teacher");
+        break;
+    default :
+        console.log("I am a Student");
+        break;
+}
